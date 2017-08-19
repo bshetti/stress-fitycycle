b78032d2f2861d47002204485ac0c0fcf8d7c601
Ubuntu <ubuntu@ip-172-27-10-118.us-west-1.compute.internal>
2017-08-19 20:28:30 +0000

    some mods

---
+++ templates/trunctable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true
+do
+
+        mysql -u db_app_user -ppassword<<EOF
+        use prospect;
+        truncate table polls_prospect;
+        alter table polls_prospect AUTO_INCREMENT = 1;
+        EOF
+
+sleep 3600
+
+done
+
