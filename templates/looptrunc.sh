245e8216207c722cb34d62ac108421fd61d7221f
Ubuntu <ubuntu@ip-172-27-10-118.us-west-1.compute.internal>
2017-08-19 21:06:02 +0000

    this adds final trun items

---
+++ templates/looptrunc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+ /home/ubuntu/trunctable.sh
+ sleep 300
+done
--- templates/trunctable.sh
+++ templates/trunctable.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-while true
-do
-
-        mysql -u db_app_user -ppassword<<EOF
-        use prospect;
-        truncate table polls_prospect;
-        alter table polls_prospect AUTO_INCREMENT = 1;
-        EOF
-
-sleep 300
-
-done
+mysql -u db_app_user -ppassword<<EOF
+use prospect;
+truncate table polls_prospect;
+alter table polls_prospect AUTO_INCREMENT = 1;
+EOF
 
